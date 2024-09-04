d624ca9283bbf818147aae73882e4ad37682acba
Samriddhi <samriddhi.maurya@axeno.co>
2024-09-04 12:38:36 +0530

    fifth

--- script.js
+++ script.js
@@ -1,30 +1,3 @@
-const accordion = document.getElementsByClassName('accordion__container');
-
-// for (i=0; i<accordion.length; i++) {
-//   accordion[i].addEventListener('click', function () {
-//     this.classList.toggle('active')
-//   })
-// }
-
-
-
-
-
-// const accordions = document.querySelectorAll('.accordion__container');
-
-// accordions.forEach((accordion) => {
-//   accordion.addEventListener('click', function () {
-//     accordions.forEach(acc => acc.classList.remove('active'));
-    
-//     document.querySelectorAll('.box').forEach(box => box.classList.remove('show'));
-
-//     this.classList.add('active');
-//     this.nextElementSibling.classList.add('show');
-//   });
-// });
-
-
-
 const accordions = document.querySelectorAll('.accordion__container');
 
 function handleClick() {
